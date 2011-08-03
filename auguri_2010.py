d5e6a0b8beaf507ec368fbc8724232ac50dfabcb
Carlo Truijllo <truijllo@crs4.it>
2011-08-03 12:10:22 +0200

    aggiunta auguri

---
+++ auguri_2010.py
@@ -0,0 +1,53 @@
+"""
+...sarebbe "normale" fare gli auguri "normalmente", per un gruppo speciale ci 
+vogliono auguri di classe.
+
+nerdometer level: 10
+requisiti:  python 
+come fa: attivare la vista "plain", copia & incolla in un file ( nomefile.py ) e
+	poi... python nomefile.py
+dove sta il divertimento: riuscite a capire cosa c'e' scritto qua sotto? :)  
+	...eppure e' python standard :)
+
+"""
+
+
+class bcolors: HEADER = '\033[95m';OKBLUE = '\033[94m';OKGREEN = '\033[92m';\
+WARNING = '\033[93m';FAIL = '\033[91m';ENDC = '\033[0m';BROWN = '\033[33m'
+sgla_pntni=10
+[#[#[#[#def disegnasgla_pntni(sgla_pntni):]#]#]#]
+]
+puntale=["|".rjust(sgla_pntni+1),"*".rjust(sgla_pntni+1),"/ \\".rjust(sgla_pntni+2),\
+"(&)".rjust(sgla_pntni+2),"|".rjust(sgla_pntni+1),"*".rjust(sgla_pntni+1)]
+"""
+legno
+"""
+ppuntale="""for i in puntale: print  bcolors.WARNING,i""" ; exec(ppuntale)
+alberale="""for i in xrange(sgla_pntni): \
+print bcolors.OKGREEN,"*".rjust(sgla_pntni-i).ljust(sgla_pntni,"*"),"*".rjust((i+1),"*")""" ; exec(alberale)
+G4m3a13=[(bcolors.BROWN+"|||\n".rjust(sgla_pntni+4))*2,bcolors.OKGREEN+"-"*\
+((sgla_pntni*2)+3)]
+"""
+natalanza
+"""
+mark="""print "_"*20,"%s"%"%s"%"f%s"%"r%s"%"o%s"%"m%s"%" %s"%"i%s"%"C%s"%"a%s"%"r%s"%"l%s"%"o","_"*20"""
+gG4m3a13="""for i in G4m3a13: print i"""; exec(gG4m3a13); print bcolors.ENDC
+[#[#[#[#def auguri():]#]#]#]
+]
+stringa=['!','raey','wen','yppah',' a','dna','samtsirhc','yrrem','a','uoy','hsiw','I']
+"""
+yuhu!
+"""
+colori=[bcolors.HEADER,bcolors.OKBLUE,bcolors.OKGREEN,bcolors.WARNING,bcolors.FAIL\
+,bcolors.BROWN,bcolors.HEADER,bcolors.OKBLUE,bcolors.OKGREEN,bcolors.WARNING,\
+bcolors.FAIL,bcolors.BROWN];out=""; 
+for i in stringa[::-1]: mappa={'col':colori[stringa.index(i)], 'dat': i[::-1]}; \
+out+="%(col)s%(dat)s " % mappa
+"""
+a
+"""
+e=chr(112)+chr(114)+chr(105)+chr(110)+chr(116)+chr(32)+chr(111)+chr(117)+chr(116)+chr(44)+\
+chr(32)+chr(98)+chr(99)+chr(111)+chr(108)+chr(111)+chr(114)+chr(115)+chr(46)+chr(69)+chr(78)+\
+chr(68)+chr(67)+chr(10); exec(e); exec(mark)
+
+
